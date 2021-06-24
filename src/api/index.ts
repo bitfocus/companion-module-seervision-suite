07489bb26e9b86427c0776de1a62db21167d3b4c
Albina Ezus <albina@seervision.com>
2021-06-24 16:40:20 +0200

    Added PtuControlManager

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,21 +1,28 @@
 import Connection from './Connection';
 import ContainersManager from './ContainersManager';
 import TrackingManager from './TrackingManager';
+import PtuControlManager from './PtuControlManager';
 
 export default class Api {
   readonly #connection: Connection;
   readonly containersManager: ContainersManager;
   readonly trackingManager: TrackingManager;
+  readonly ptuControlManager: PtuControlManager;
 
   constructor(dopIp: string, onUpdate: () => void) {
     this.#connection = new Connection(dopIp);
 
     this.containersManager = new ContainersManager(this.#connection, onUpdate);
     this.trackingManager = new TrackingManager(this.#connection, onUpdate);
+    this.ptuControlManager = new PtuControlManager(this.#connection, onUpdate);
   }
 
   init(): Promise<Array<void>> {
-    return Promise.all([this.containersManager.init(), this.trackingManager.init()]);
+    return Promise.all([
+      this.containersManager.init(),
+      this.trackingManager.init(),
+      this.ptuControlManager.init(),
+    ]);
   }
 
   closeConnection(): void {

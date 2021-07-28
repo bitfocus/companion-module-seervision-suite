edbd7ecabd73c10430c47d37a47a295ba50b90b5
Florian Gutzwiller <florian.gutzwiller@gmx.ch>
2021-07-28 15:37:58 +0200

    refactor by sticking to alphabetical ordering

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,31 +1,31 @@
 import Connection from './Connection';
 import ContainersManager from './ContainersManager';
-import TrackingManager from './TrackingManager';
 import PtuControlManager from './PtuControlManager';
 import TallyManager from './TallyManager';
+import TrackingManager from './TrackingManager';
 
 export default class Api {
   readonly #connection: Connection;
   readonly containersManager: ContainersManager;
-  readonly trackingManager: TrackingManager;
   readonly ptuControlManager: PtuControlManager;
   readonly tallyManager: TallyManager;
+  readonly trackingManager: TrackingManager;
 
   constructor(dopIp: string, onUpdate: () => void) {
     this.#connection = new Connection(dopIp);
 
     this.containersManager = new ContainersManager(this.#connection, onUpdate);
-    this.trackingManager = new TrackingManager(this.#connection, onUpdate);
     this.ptuControlManager = new PtuControlManager(this.#connection, onUpdate);
     this.tallyManager = new TallyManager(this.#connection, onUpdate);
+    this.trackingManager = new TrackingManager(this.#connection, onUpdate);
   }
 
   init(): Promise<Array<void>> {
     return Promise.all([
       this.containersManager.init(),
-      this.trackingManager.init(),
       this.ptuControlManager.init(),
       this.tallyManager.init(),
+      this.trackingManager.init(),
     ]);
   }
 
